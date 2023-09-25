b231774bb4394e66649ce6689cb93267b57b66bc
ElielsonU <elielson7756@gmail.com>
2023-09-24 23:38:37 -0300

    mudanças

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,62 +1,70 @@
 "use client";
 import { GlobalStyle } from "@/components/globalstyle";
 import { Header } from "@/components/standards";
-import styled from 'styled-components';
+import styled from "styled-components";
 
 const Desciption = styled.div`
-    width: 481px;
-    height: 533px;
-    display: flex;
-    flex-direction: column;
-    
-
+  width: 481px;
+  height: 533px;
+  display: flex;
+  flex-direction: column;
 `;
 
 const Main = styled.main`
-    display: flex;
+  display: flex;
 `;
 
 const Botao = styled.button`
-    width: 481px;
-    height: 70px;
-    background-color: #5BC0BE;
-    border: none;
-    border-radius: 12.07px;
-    color: white;
-    font-size: 3.5rem;
-    margin: 0;
-    padding: 0;
-    font-weight: bold;
+  width: 481px;
+  height: 70px;
+  background-color: #5bc0be;
+  border: none;
+  border-radius: 12.07px;
+  color: white;
+  font-size: 3.5rem;
+  margin: 0;
+  padding: 0;
+  font-weight: bold;
 `;
 
 const Text = styled.h2`
-    color: #0B132B;
-    font-size: 2.6rem;
+  color: #0b132b;
+  font-size: 2.6rem;
 `;
 
 const Link = styled.h2`
-    font-size: 2.6rem;
-    margin: 30px 0px 30px 0px;
+  font-size: 2.6rem;
+  margin: 30px 0px 30px 0px;
 `;
 
 export default function Home() {
   return (
     <>
       <GlobalStyle />
-      <Header type="light"/>
+      <Header type="light" />
       <Main>
         <>
           <Desciption>
-            <Text>Create your own autobiography with
-our services. Here, without any knowledge about how to make a biography, you can have a professional work, made with AIs customized to you. The best part is: you can do all of this just speaking, we implemented a way to help people that have certain deficiencies creating your autobiography. An addiotional is: we can restore your old photos here and place it on your autobiography automatic, giving more originality to your project</Text>
-            <Link><a href="*">How to use it?</a></Link>
+            <Text>
+              Create your own autobiography with our services. Here, without any
+              knowledge about how to make a biography, you can have a
+              professional work, made with AIs customized to you. The best part
+              is: you can do all of this just speaking, we implemented a way to
+              help people that have certain deficiencies creating your
+              autobiography. An addiotional is: we can restore your old photos
+              here and place it on your autobiography automatic, giving more
+              originality to your project
+            </Text>
+            <Link>
+              <a href="*">How to use it?</a>
+            </Link>
             <Botao>Start Now!</Botao>
           </Desciption>
-          <img  src="/imgs/mão.png"/>
+          <img src="/imgs/mão.png" />
         </>
       </Main>
     </>
   );
 }
 
-''
+("");
