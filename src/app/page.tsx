96faf8b2e65c8f365f4c344f3e5fde462f217795
Abell112233 <abelllfineghan@gmail.com>
2023-09-24 23:00:52 -0300

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,49 @@
 "use client";
 import { GlobalStyle } from "@/components/globalstyle";
 import { Header } from "@/components/standards";
+import styled from 'styled-components';
+
+const Desciption = styled.div`
+    width: 481px;
+    height: 390px;
+    display: flex;
+    flex-direction: column;
+    
+
+`;
+
+const Main = styled.main`
+    display: flex;
+`;
+
+const Botao = styled.button`
+    width: 481px;
+    background-color: #5BC0BE;
+    border: none;
+    border-radius: 12.07px;
+    color: black;
+    font-size: 4rem;
+    margin: 0;
+    padding: 0;
+`;
 
 export default function Home() {
   return (
     <>
-      <GlobalStyle />
       <Header type="light"/>
-      <h1></h1>
+      <Main>
+        <>
+          <Desciption>
+            <h2>Create your own autobiography with
+our services. Here, without any knowledge about how to make a biography, you can have a professional work, made with AIs customized to you. The best part is: you can do all of this just speaking, we implemented a way to help people that have certain deficiencies creating your autobiography. An addiotional is: we can restore your old photos here and place it on your autobiography automatic, giving more originality to your project</h2>
+            <h2><a href="*">How to use it?</a></h2>
+            <Botao><p>Start Now!</p></Botao>
+          </Desciption>
+          <img  src="/imgs/mão.png"/>
+        </>
+      </Main>
     </>
   );
 }
+
+''
